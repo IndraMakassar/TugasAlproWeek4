78a93342acdae51c5404cdb15abafd7c47723b91
Antonius Indra Dharma Prasetya <anindhpra@gmail.com>
2022-10-20 14:27:51 +0800

    menambah variable

--- Antonius_Indra_Dharma_Prasetya_0806022210004_Alpro_Nomor_2.java
+++ Antonius_Indra_Dharma_Prasetya_0806022210004_Alpro_Nomor_2.java
@@ -4,6 +4,11 @@ public class Antonius_Indra_Dharma_Prasetya_0806022210004_alpro_Nomor_2 {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        System.out.print("Nilai : ");
+        float N = input.nextFloat();
+
         
+
     }
 }
