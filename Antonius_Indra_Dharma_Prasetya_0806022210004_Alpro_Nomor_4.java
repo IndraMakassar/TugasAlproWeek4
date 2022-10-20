96b8b2afd9a661219ca775c3d57f73caabe2dcab
Antonius Indra Dharma Prasetya <anindhpra@gmail.com>
2022-10-20 13:55:57 +0800

    Menambah template nomor 1-5

---
+++ Antonius_Indra_Dharma_Prasetya_0806022210004_Alpro_Nomor_4.java
@@ -0,0 +1,9 @@
+import.java.util.Scanner;
+
+public class Antonius_Indra_Dharma_Prasetya_0806022210004_alpro_Nomor_4 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+    }
+}
