5947b0fb4e9a2d25c6ab5f7206d03efe3783db55
Antonius Indra Dharma Prasetya <anindhpra@gmail.com>
2022-10-20 14:54:27 +0800

    Menyelesaikan nomor 4

--- Antonius_Indra_Dharma_Prasetya_0806022210004_Alpro_Nomor_4.java
+++ Antonius_Indra_Dharma_Prasetya_0806022210004_Alpro_Nomor_4.java
@@ -3,7 +3,27 @@ import.java.util.Scanner;
 public class Antonius_Indra_Dharma_Prasetya_0806022210004_Alpro_Nomor_4 {
 
     public static void main(String[] args) {
+        float GajiKotor, Pajak;
+        
         Scanner input = new Scanner(System.in);
+
+        System.out.print("Total jam kerja : ");
+        int Jam = input.nextInt();
+        
+        if (Jam >= 40){
+            GajiKotor = (float) (Jam * 6 * 1.5);
+        } else {
+            GajiKotor = (float) (Jam * 6);
+        }
+        
+        if (GajiKotor >= 250){
+            Pajak = GajiKotor * 12/100;
+        } else {
+            Pajak = GajiKotor * 10/100;
+        }
+        System.out.println("Gaji Kotor : " + GajiKotor);
+        System.out.println("Pajak : " + Pajak);
+        System.out.println("Gaji Bersih : " + (GajiKotor - Pajak));
         
     }
 }
