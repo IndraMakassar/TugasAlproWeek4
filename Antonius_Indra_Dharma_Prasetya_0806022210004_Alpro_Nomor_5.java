5e3edc174137233720fd0dbb965690d4d35d881c
Antonius Indra Dharma Prasetya <anindhpra@gmail.com>
2022-10-20 15:03:14 +0800

    menyelesaikan nomor 5

--- Antonius_Indra_Dharma_Prasetya_0806022210004_Alpro_Nomor_5.java
+++ Antonius_Indra_Dharma_Prasetya_0806022210004_Alpro_Nomor_5.java
@@ -4,6 +4,23 @@ public class Antonius_Indra_Dharma_Prasetya_0806022210004_Alpro_Nomor_5 {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        System.out.print("Nama Anda : ");
+        String Nama = input.nextLine();
+        
+        System.out.print("Jenis Kelamin (L/P) : ");
+        String Jenis = input.nextLine();
+        
+        System.out.print("Menikah (Y/T)? :");
+        String Menikah = input.nextLine();
+        
+        if (Jenis.equalsIgnoreCase("l")){
+            System.out.println("Hello, Mr." + Nama);
+        } else if (Jenis.equalsIgnoreCase("P") && Menikah.equalsIgnoreCase("Y")){
+            System.out.println("Hello, Mrs. " + Nama);
+        } else {
+            System.out.println("Hello, Ms. " + Nama);
+        }
         
     }
 }
