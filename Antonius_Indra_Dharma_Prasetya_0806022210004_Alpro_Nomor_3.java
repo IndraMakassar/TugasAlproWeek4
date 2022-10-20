6bcc1f97346f76688e2134e9eda0c7ea87336c99
Antonius Indra Dharma Prasetya <anindhpra@gmail.com>
2022-10-20 14:34:24 +0800

    menyelesaikan nomor 3

--- Antonius_Indra_Dharma_Prasetya_0806022210004_Alpro_Nomor_3.java
+++ Antonius_Indra_Dharma_Prasetya_0806022210004_Alpro_Nomor_3.java
@@ -5,5 +5,13 @@ public class Antonius_Indra_Dharma_Prasetya_0806022210004_alpro_Nomor_3 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         
+        System.out.print("Angka : ");
+        int N = input.nextInt();
+        
+        if (N % 2 == 0){
+            System.out.println("Genap");
+        } else {
+            System.out.println("Ganjil");
+        }
     }
 }
